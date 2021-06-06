ff904b946a34ef9dda9ed1ced416661a54ee2ad6
David Santamaria <davidsantamaria1245@gmail.com>
2021-06-06 19:57:28 -0400

    Fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,25 +6,37 @@ import java.util.Scanner;
  */
 public class Main {
 
+    static Scanner in = new Scanner(System.in);
+
     public static double bmi(int weight, int height) {
         return ((double) weight / (height * height)) * 703;
     }
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    public static int ensure_number() {
+        while (true) {
+            try {
+                return in.nextInt();
+            } catch (Exception e) {
+                System.out.println("You must enter a numeric value!");
+                in.nextLine();
+            }
+        }
+    }
 
+    public static void main(String[] args) {
         System.out.print("What is the weight?");
-        int weight = in.nextInt();
+        int weight = ensure_number();
         System.out.print("What is the height?");
-        int height = in.nextInt();
+        int height = ensure_number();
         double bmi = bmi(weight, height);
         System.out.printf("Your BMI is %.1f\n", bmi);
         if (bmi <= 18.5) {
             System.out.println("You are underweight.");
-        } if (bmi >= 25) {
+        } else if (bmi >= 25) {
             System.out.println("You are overweight. You should see your doctor.");
         } else {
             System.out.println("You are within the ideal weight range.");
         }
+        in.close();
     }
 }
